a378db2a05ba2596e10584b8d0b21df04a6441ad
Rijman303 <rijmana703@gmail.com>
2023-05-29 21:23:36 +0545

    changing server architecture

---
+++ server/src/controller/product.js
@@ -0,0 +1,16 @@
+const Product = require ('../model/product')
+const getAllProducts =async (req,res)=>{
+    const data = await Product.find()
+    res.json({productList:data})
+  }
+
+  const addNewProducts =async (req,res)=>{
+    const data = await Product.create(req.body)
+    if(data) {
+      res.json({
+        msg: "product add success"
+      })
+    }
+  }
+
+  module.exports ={getAllProducts,addNewProducts}
---
+++ server/src/routes/product.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express.Router()
+const ProductController = require ('../controller/product')
+
+app.post('/products',ProductController.addNewProducts)
+app.get('/products',ProductController.getAllProducts)
+
+module.exports=app;
