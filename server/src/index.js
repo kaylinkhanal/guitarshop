a21b07907ca3cc6682df5dd2ba3fb89f5bfef36d
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-05-29 10:41:49 +0545

    changed server architecture MVC

--- server/src/index.js
+++ server/src/index.js
@@ -2,31 +2,13 @@ const express = require('express')
 const app = express()
 const cors =require('cors')
 require('dotenv').config()
-///parse body
 app.use(express.json())
 app.use(cors())
 const connectDb = require('./dbConnect/connection')
-const Product = require('./model/product')
-
+const productRoute=require('./routes/product')
 
 connectDb()
-
-
-
-app.post('/products',async (req,res)=>{
-  const data = await Product.create(req.body)
-  if(data) {
-    res.json({
-      msg: "product add success"
-    })
-  }
-})
-
-
-app.get('/products',async (req,res)=>{
-  const data = await Product.find()
-  res.json({productList:data})
-})
+app.use("/",productRoute)
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
