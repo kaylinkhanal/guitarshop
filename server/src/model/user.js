c7ec8ab08705ddaaecb5d81f97632985ffc35067
Prasanna Gadal <gadalprasanna5@gmail.com>
2023-06-02 09:44:43 +0545

    merging

--- server/src/model/user.js
+++ server/src/model/user.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-//define shape of the document
-const productSchema =  new mongoose.Schema({
-    userName: String,
-    password: String,
-    confirmPassword: String
-    });
-  const Register = mongoose.model('Register', productSchema);
-
-  
-  module.exports = Register
-=======
 const mongoose= require('mongoose')
 
 const userSchema =  new mongoose.Schema({
@@ -24,4 +11,3 @@ const userSchema =  new mongoose.Schema({
 
   
 module.exports = User
->>>>>>> ca387dfb35e96e282d4e10ef5f313461ee32a826
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,11 +1,7 @@
 const express=require('express')
-<<<<<<< HEAD
-const app = express.Router()
 
 
 
-module.exports = app;
-=======
 const app=express.Router()
 const UserController = require('../controller/user')
 app.post('/register', UserController.registerNewUser)
@@ -14,4 +10,4 @@ app.get('/users', UserController.getAllUser)
 
 
 module.exports=app;
->>>>>>> ca387dfb35e96e282d4e10ef5f313461ee32a826
+
